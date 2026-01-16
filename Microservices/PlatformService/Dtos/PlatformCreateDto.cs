3bb41f35a518887815bf26bdaf66dc3c369c95d9
Bernie-ramone <bernie_ramone@outlook.com>
2026-01-16 07:45:33 -0600

    add data an mapper

---
+++ Microservices/PlatformService/Dtos/PlatformCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Dtos
+{
+    public class PlatformCreateDto
+    {
+        public required string Name { get; set; }
+        public required string Publisher { get; set; }
+        public required string  Cost { get; set; }
+    }
+}
--- Microservices/PlatformService/Program.cs
+++ Microservices/PlatformService/Program.cs
@@ -1,9 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseInMemoryDatabase("InMem"));
+
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.AddMaps(AppDomain.CurrentDomain.GetAssemblies());
+});
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -19,5 +33,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+PrepDb.PrepPopulation(app);
 
 app.Run();
