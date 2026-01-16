f5f1f921ea757f1cae57454e9da574c1aa935089
Bernie-ramone <bernie_ramone@outlook.com>
2026-01-16 08:21:06 -0600

    add controller

---
+++ Microservices/PlatformService/Controllers/PlatformController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformController(IPlatformRepo repository, IMapper mapper) : ControllerBase
+    {
+        private readonly IPlatformRepo repository = repository;
+        private readonly IMapper mapper = mapper;
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms...");
+
+            var platformItems = repository.GetAllPlatforms();
+
+            return Ok(mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+        }
+
+    }
+}
